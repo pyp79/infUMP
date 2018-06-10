067c797f296f7a385502f0687eff582f196cf89f
pyp79 <pyp79@sina.com>
2018-06-10 21:59:58 +0800

    20180610

---
+++ venv/bin/export_cmdb_umpdoc.py
@@ -0,0 +1,51 @@
+1  #!/usr/bin/python3
+2  # -*- coding:utf-8 -*- 
+3  #Author: Pang Yapeng
+
+import datetime
+import csv
+from sub_common import *
+
+# 特定参数
+conf = get_conf()
+EXPORT_FILE = conf.get("EXPORT_FILE", "UMP_DOC_FILE")
+TABLE_NAME = "UMP_DOC"
+
+# 设置log
+logger = get_logger("export.log")
+
+def main():
+    logger.info("Begin to export from table:%s",TABLE_NAME)
+
+    v_date_para = re.findall(r"\[(.*)\]", EXPORT_FILE)[0]
+    new_date = datetime.datetime.now().strftime(v_date_para)
+    EXPORT_FILE_NEW = re.sub("\[.*\]", new_date, EXPORT_FILE)
+
+    # 查数据
+    conn = get_conn()
+    cursor = conn.cursor();
+    sql = "SELECT APP_NAME,IP_ADDRESS,POLICY_NAME,POLICY_DESC,COMPONENT_TYPE,MONITOR," \
+          "SEV1_CONDITION,SEV2_CONDITION,SEV3_CONDITION FROM {0}".format(TABLE_NAME)
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    conn.close()
+
+    # 生成csv文件
+    csv_file = open(EXPORT_FILE_NEW,'w',newline='')
+    writer = csv.writer(csv_file)
+    writer.writerow(['APP_NAME','IP_ADDRESS','POLICY_NAME','POLICY_DESC','COMPONENT_TYPE','MONITOR','SEV1_CONDITION','SEV2_CONDITION','SEV3_CONDITION'])
+    writer.writerows(rows)
+    csv_file.close()
+    logger.info("Success write file:%s.",EXPORT_FILE_NEW)
+
+    # ftp到服务器
+    ftp = get_cmdb_ftp()
+    filename = os.path.basename(EXPORT_FILE_NEW)
+    handle = open(EXPORT_FILE_NEW, "rb")
+    ftp.storbinary("STOR /TEST/" + filename, handle, 1024)
+    handle.close()
+
+    logger.info("Success ftp the file:%s.",EXPORT_FILE_NEW)
+
+if __name__ == '__main__':
+    main()
---
+++ venv/bin/sub_common.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import re
+import sys
+import logging
+import pymysql
+import os
+import configparser
+import ftplib
+
+def get_conf():
+    # read config
+    cur_path=os.path.dirname(os.path.realpath(__file__))
+    config_path=os.path.join(cur_path,'../conf/config.ini')
+    conf = configparser.ConfigParser()
+    conf.read(config_path,encoding='utf-8-sig')
+    return conf
+
+def get_cmdb_ftp():
+    # get cmdb ftp 连接
+    conf = get_conf()
+    FTP_CMDB_IP = conf.get("CMDB_FTP", "FTP_CMDB_IP")
+    FTP_CMDB_PORT = conf.get("CMDB_FTP", "FTP_CMDB_PORT")
+    FTP_CMDB_USER = conf.get("CMDB_FTP", "FTP_CMDB_USER")
+    FTP_CMDB_PASSWD = conf.get("CMDB_FTP", "FTP_CMDB_PASSWD")
+    ftp = ftplib.FTP()
+    ftp.connect(FTP_CMDB_IP, int(FTP_CMDB_PORT))
+    ftp.login(FTP_CMDB_USER, FTP_CMDB_PASSWD)
+    return ftp
+
+
+def get_conn():
+    conf = get_conf()
+    DB_IP = conf.get("MYSQL", "DB_IP")
+    DB_PORT = conf.get("MYSQL", "DB_PORT")
+    DB_USER = conf.get("MYSQL", "DB_USER")
+    DB_PASSWORD = conf.get("MYSQL", "DB_PASSWORD")
+    DB_SCHEMA = conf.get("MYSQL", "DB_SCHEMA")
+    DB_CHARSET = conf.get("MYSQL", "DB_CHARSET")
+    conn = pymysql.connect(host=DB_IP,port=int(DB_PORT),user=DB_USER,passwd=DB_PASSWORD,db=DB_SCHEMA,use_unicode=True,charset=DB_CHARSET)
+    return(conn)
+
+def clean_db(sql):
+    conn = get_conn()
+    cursor = conn.cursor();
+    cursor.execute(sql);
+    conn.commit();
+
+def get_logger(FILENAME):
+    conf = get_conf()
+    LOG_FILE =os.path.join(conf.get("LOG_FILE", "LOG_PATH") , FILENAME)
+
+    # 设置log
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(LOG_FILE,encoding="UTF-8")
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+def isIP(str): #判断字符串是否IP地址格式
+    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    if p.match(str):
+        return True
+    else:
+        return False
